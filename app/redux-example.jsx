c1f22bd5042b745ce3ad32556bc3872cbf81205b
dougwells <dougwells@gmail.com>
2016-06-22 14:30:09 -0600

    Redux & Reuse Example: Change Name

---
+++ app/redux-example.jsx
@@ -0,0 +1,30 @@
+var redux = require('redux');
+
+console.log("Redux-example operational");
+
+var reducer = (state={name: "Anonymous"}, action)=>{
+  // state = state || {name: "Anonymous"}
+  console.log("New action: ", action);
+
+  switch(action.type){
+    case 'CHANGE_NAME':
+      return {...state, name: action.name}
+
+    default:
+      return state;
+  };
+
+};
+
+var store = redux.createStore(reducer)
+
+var currentState = store.getState();
+console.log('Current State = ', currentState);
+
+var action = {
+  type: "CHANGE_NAME",
+  name: "Doug"
+};
+
+store.dispatch(action);
+console.log(store.getState());
