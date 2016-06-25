a29880d1f621256905988121b5b9b53d7c5ab603
dougwells <dougwells@gmail.com>
2016-06-25 17:40:45 -0600

    Refactored into separate files for reducers, actions and the store

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,141 +1,13 @@
 var redux = require('redux');
-var axios = require('axios');
-
-// Name Reducer and action generators
-// -----------------------
-var nameReducer = (state="Anonymous", action) =>{
-  switch(action.type){
-    case 'CHANGE_NAME':
-      return action.name;
-
-      default: return state;
-  }
-};
-
-var changeName = (name)=> {
-  return {
-    type: 'CHANGE_NAME',
-    name
-  }
-};
-
-// Hobby Reducer and action generators
-// -----------------------
-var nextHobbyId = 1;
-var hobbyReducer = (state=[], action) =>{
-  switch(action.type){
-    case 'ADD_HOBBY':
-      return [...state,
-     {
-       id: nextHobbyId++,
-       hobby: action.hobby
-     }
-   ];
-   case "REMOVE_HOBBY":
-      return state.filter((hobby)=> hobby.id !== action.id);
-
-    default:
-      return state;
-       };
- };
-
- //Actions Generators - Hobbies
-var addHobby = (hobby)=>{
-  return {
-  type: 'ADD_HOBBY',
-  hobby
-  }
-};
-var removeHobby = (id)=>{
-  return {
-    type: "REMOVE_HOBBY",
-    id
-  }
-};
 
 
- // Movie Reducer and action generators
- // -----------------------
-var nextMovieId = 1
- var movieReducer = (state=[], action) => {
-   switch(action.type){
-     case "ADD_MOVIE":
-        return [...state, {id: nextMovieId++, title: action.title, genre: action.genre}];
+console.log("Starting Redux Example App");
 
-    case "REMOVE_MOVIE":
-        return state.filter((movie)=>{return movie.id !== action.id});
-
-    default: return state;
-   }
- };
+var actions = require('./actions/index');
+var store = require('./store/configureStore').configure();
+var reducers = require('./reducers/index');
 
 
-
-//Actions Generators - Movies
-     var addMovie = (title, genre) => {
-       return {
-         type: "ADD_MOVIE",
-         title,
-         genre
-       }
-     };
-
-     var removeMovie = (id) => {
-       return {
-         type: "REMOVE_MOVIE",
-         id
-       }
-     };
-
-// Map Reducer and action generators
-// -----------------------
-var mapReducer = (state = {isFetching: false, url: undefined}, action) => {
-  switch(action.type){
-    case 'START_LOCATION_FETCH':
-      return {isFetching: true, url: undefined};
-
-    case 'COMPLETE_LOCATION_FETCH':
-      return {isFetching: false, url: action.url};
-
-    default: return state;
-  }
-};
-
-//Actions Generators - Map
-var startLocationFetch = () => {
-  return {
-    type: 'START_LOCATION_FETCH'
-  };
-};
-
-var completeLocationFetch = (url) => {
-  return {
-    type: 'COMPLETE_LOCATION_FETCH',
-    url
-  };
-};
-
-var fetchLocation = () =>{
-  store.dispatch(startLocationFetch());
-
-    axios.get('http://ipinfo.io').then(function(res){
-      var loc = res.data.loc;
-      var baseUrl = "http://maps.google.com/?q="
-      store.dispatch(completeLocationFetch(baseUrl+loc))
-    });
-
-};
-
-var reducer = redux.combineReducers({
-  name: nameReducer,
-  hobbies: hobbyReducer,
-  movies: movieReducer,
-  map: mapReducer
-});
-
-var store = redux.createStore(reducer, redux.compose(
-  window.devToolsExtension ? window.devToolsExtension() : f => f
-));
 
 var state = store.getState();
 document.getElementById('app').innerHTML = state.name;
@@ -154,13 +26,12 @@ var unsubscribe = store.subscribe(()=>{
 });
 // unsubscribe();
 
-fetchLocation();
-
-store.dispatch(changeName('Doug'));
-store.dispatch(addHobby('Skiing'));
-store.dispatch(addHobby('Surfing'));
-store.dispatch(removeHobby(2));
-store.dispatch(changeName('Ted'));
-store.dispatch(addMovie('Rocky', 'Romance'));
-store.dispatch(addMovie('Rocky II', 'Action'));
-store.dispatch(removeMovie(2));
+store.dispatch(actions.fetchLocation());
+store.dispatch(actions.changeName('Doug'));
+store.dispatch(actions.addHobby('Skiing'));
+store.dispatch(actions.addHobby('Surfing'));
+store.dispatch(actions.removeHobby(2));
+store.dispatch(actions.changeName('Ted'));
+store.dispatch(actions.addMovie('Rocky', 'Romance'));
+store.dispatch(actions.addMovie('Rocky II', 'Action'));
+store.dispatch(actions.removeMovie(2));
